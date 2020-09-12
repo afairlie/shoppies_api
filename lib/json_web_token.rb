9ea4ecdbda9b26a1d37900142f1fb6a182783c17
Ariane <ariane.fairlie@gmail.com>
2020-09-12 14:37:05 -0400

    fixed env bug, credentials envrypted

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -9,16 +9,16 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithms: 'RS256',
-               iss: ENV['AUTH0_DOMAIN'],
+               iss: Rails.application.credentials.dig(:auth0_domain),
                verify_iss: true,
-               aud: ENV['AUTH0_API_AUDIENCE'],
+               aud: Rails.application.credentials.dig(:auth0_api_audience),
                verify_aud: true) do |header|
       jwks_hash[header['kid']]
     end
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{ENV['AUTH0_DOMAIN']}.well-known/jwks.json") #BUG: ENV doesn't work here
+    jwks_raw = Net::HTTP.get URI("#{Rails.application.credentials.dig(:auth0_domain)}.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
