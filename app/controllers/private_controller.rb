991e65e3840947a8e854868200333cab7f996be7
Ariane <ariane.fairlie@gmail.com>
2020-09-11 22:55:41 -0400

    auth0 public and private test routes configed. Bug: env not working in json_web_token ln 21

---
+++ app/controllers/private_controller.rb
@@ -0,0 +1,9 @@
+# app/controllers/private_controller.rb
+
+class PrivateController < ActionController::API
+  include Secured
+
+  def private
+    render json: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'api/public' => 'public#public'
+  get 'api/private' => 'private#private'
 end
