64d8daff69908b76700d0849c987b370f73127d5
Ariane <ariane.fairlie@gmail.com>
2020-09-15 13:34:55 -0400

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  # read in the user params
+  # create a new user with them
+  # send back created user
+
+  def create
+    user = User.create(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    render json: user
+  end
 end
