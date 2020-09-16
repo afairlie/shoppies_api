0e2bcf45744c948dc910f87053796c8d4e4ad631
Ariane <ariane.fairlie@gmail.com>
2020-09-16 17:44:10 -0400

    issue token on user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
       password: params[:password]
     )
     if user 
-      render json: user
+      payload = {
+        id: user.id,
+        email: user.email,
+        exp: 5.minutes.from_now.to_i }
+      token = JWT.encode(payload, Rails.application.credentials.dig(:secret_key_base))
+      render json: { token: token, username: user.name }
     else 
       render json: { message: 'Failed to create user' }
     end
