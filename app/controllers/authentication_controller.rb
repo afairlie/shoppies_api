eda6f2521abafca49ec213f2dc4238e06d10df6a
Ariane <ariane.fairlie@gmail.com>
2020-09-17 15:18:36 -0400

    error handling fixes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,17 +5,19 @@ class AuthenticationController < ApplicationController
     if !user
       render json: { error: "Invalid email"},
       status: :unauthorized
-    elsif user.authenticate(params[:password])
-      payload = {
-        id: user.id,
-        email: user.email,
-        exp: 5.minutes.from_now.to_i }
-      token = JWT.encode(payload, Rails.application.credentials.dig(:secret_key_base))
-      # is it ok to send username in open, or do I need to keep in token payload and decode?
-      render json: { token: token, username: user.name }
-    else 
-      render json: { message: "Incorrect password :("},
-      status: :unauthorized
+    else
+      if user.authenticate(params[:password])
+        payload = {
+          id: user.id,
+          email: user.email,
+          exp: 5.minutes.from_now.to_i }
+        token = JWT.encode(payload, Rails.application.credentials.dig(:secret_key_base))
+        # is it ok to expose username, or do I need to keep in token payload and decode?
+        render json: { token: token, username: user.name, nominations: user.nomination }
+      else 
+        render json: { error: "Incorrect password"},
+        status: :unauthorized
+      end
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
   def create
-    user = User.create(
+    @user = User.new(
       name: params[:name],
       email: params[:email],
       password: params[:password]
     )
-    if user 
+    if @user.save 
       payload = {
-        id: user.id,
-        email: user.email,
+        id: @user.id,
+        email: @user.email,
         exp: 5.minutes.from_now.to_i }
       token = JWT.encode(payload, Rails.application.credentials.dig(:secret_key_base))
-      render json: { token: token, username: user.name }
+      render json: { token: token, username: @user.name }
     else 
-      render json: { message: 'Failed to create user' }
+      render json: { error: 'Failed to create user' }, status: :unauthorized
     end
   end
 end
